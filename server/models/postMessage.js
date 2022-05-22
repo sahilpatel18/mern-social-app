dfc1b6545ee15ded1be95f267c9350fb37d6eb62
sahilpatel18 <sahil.41801@gmail.com>
2022-05-22 15:35:28 -0400

    mongoose schema for posts

---
+++ server/models/postMessage.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: { type: Number, default: 0 },
+  createdAt: { type: Date, default: new Date() },
+});
+
+const postMessage = mongoose.model("PostMessage", postSchema);
+
+export default postMessage;
