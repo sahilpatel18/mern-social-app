a02081222b1a51e9ca92b20e64cd54fd89815773
sahilpatel18 <sahil.41801@gmail.com>
2022-05-22 15:35:02 -0400

    importing the getPosts logic to the get request

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,3 +1,8 @@
-import express from 'express'
+import express from "express";
+import { getPosts } from "../controllers/posts.js";
 
 const router = express.Router();
+
+router.get("/", getPosts);
+
+export default router;
