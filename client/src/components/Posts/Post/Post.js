fd280ec90792a23de7d698516eb006685d30acf2
sahilpatel18 <sahil.41801@gmail.com>
2022-05-31 15:13:20 -0400

    mapping user post submitions

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,73 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
 import useStyles from "./styles";
 
-
-const Post = () => {
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <h1> Post</h1>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        component="img"
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography varient="h6">{post.creator}</Typography>
+        <Typography varient="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="medium" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography varient="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag}`)}
+        </Typography>
+      </div>
+      <CardContent>
+        <Typography className={classes.title} varient="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {
+            console.log("hello");
+          }}
+        >
+          <ThumbUpAltIcon fontSize="small" />
+          {post.likeCount}
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => {
+            console.log("hello");
+          }}
+        >
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
