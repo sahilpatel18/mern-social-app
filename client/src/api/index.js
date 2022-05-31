b9e8105868cb503d3c75e4b04776ae4165e15d54
sahilpatel18 <sahil.41801@gmail.com>
2022-05-31 17:08:32 -0400

    update post api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const { REACT_APP_DEV_URL } = process.env;
 
-export const fetchPost =  () => axios.get(REACT_APP_DEV_URL);
+export const fetchPost = () => axios.get(REACT_APP_DEV_URL);
 export const createPost = (newPost) => axios.post(REACT_APP_DEV_URL, newPost);
+export const updatePost = (id, updatedPost) =>
+  axios.patch(`${REACT_APP_DEV_URL}/${id}`, updatedPost);
