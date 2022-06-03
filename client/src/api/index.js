0f594f56a7dc42d41eecbf6ee09338a61f24a533
sahilpatel18 <sahil.41801@gmail.com>
2022-06-03 17:33:31 -0400

    delete post api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,5 +3,9 @@ const { REACT_APP_DEV_URL } = process.env;
 
 export const fetchPost = () => axios.get(REACT_APP_DEV_URL);
 export const createPost = (newPost) => axios.post(REACT_APP_DEV_URL, newPost);
-export const updatePost = (id, updatedPost) =>
+export const updatePost = (id, updatedPost) => {
   axios.patch(`${REACT_APP_DEV_URL}/${id}`, updatedPost);
+};
+export const deletePost = (id) => {
+  axios.delete(`${REACT_APP_DEV_URL}/${id}`);
+};
