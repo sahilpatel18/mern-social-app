8c7c8c1e43132764f8a9247a59ebe9ba48729085
sahilpatel18 <sahil.41801@gmail.com>
2022-06-03 17:31:56 -0400

    update post route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -25,11 +25,25 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
-  if (mongoose.types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(_id.toString()))
     return res.status(404).send("No post with that id");
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  );
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id.toString()))
+    return res.status(404).send("No post with that id");
+
+  await PostMessage.findByIdAndRemove(id);
+  console.log("deleted post " + id);
+  res.json({ message: "Post deleted successfully!" });
 };
