4e898e0afc4ae1ca2e0223789fb3eaa26f4df62a
sahilpatel18 <sahil.41801@gmail.com>
2022-06-03 17:32:17 -0400

    created delete post reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,6 +4,13 @@ export default (posts = [], action) => {
       return action.payload;
     case "CREATE":
       return [...posts, action.payload];
+    case "UPDATE":
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case "DELETE":
+      return posts.filter((post) => post._id !== action.payload);
+      return;
     default:
       return posts;
   }
